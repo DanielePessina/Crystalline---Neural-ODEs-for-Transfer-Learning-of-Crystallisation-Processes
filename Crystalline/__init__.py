4bdb8a5c393dc4786a35618222ca18a8d005dddb
Daniele Pessina <daniele.pessina18@imperial.ac.uk>
2025-11-23 14:20:56 +0000

    super commit

--- Crystalline/__init__.py
+++ Crystalline/__init__.py
@@ -7,9 +7,6 @@ imported directly from :mod:`Crystalline`.
 
 # from .augmented import train_AugNODE_fresh, train_AugNODE_TL, train_AugNODE_TL_penalise_deviation
 from .augmented import train_AugNODE_fresh, train_AugNODE_TL, train_AugNODE_TL_penalise_deviation
-from .augmented_domain import (
-    train_SystemAugNODE_fresh,
-)
 from .augmented_domain_learnable import create_system_embedding, train_SystemAugNODElearnable_fresh
 from .constrained import train_AugNODEconstrained_fresh, train_AugNODEconstrained_TL
 from .data_functions import (
@@ -22,3 +19,19 @@ from .plotting import (
     splitplot_model_vs_data,
     splitplot_model_vs_data_1d,
 )
+
+_all__ = [
+    "train_AugNODE_fresh",
+    "train_AugNODE_TL",
+    "train_AugNODE_TL_penalise_deviation",
+    "create_system_embedding",
+    "train_SystemAugNODElearnable_fresh",
+    "train_AugNODEconstrained_fresh",
+    "train_AugNODEconstrained_TL",
+    "simulateCrystallisation",
+    "calculate_all_metrics",
+    "plot_ensemble_predictions",
+    "plot_loss_curves",
+    "splitplot_model_vs_data",
+    "splitplot_model_vs_data_1d",
+]
